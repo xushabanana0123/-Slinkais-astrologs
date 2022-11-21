aa8b65666fd0728bcb776e3d409b4181955ed97f
kderune <91871014+xushabanana0123@users.noreply.github.com>
2022-11-21 07:04:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Slinkais astrologs
+name = input('Libra')
+print("You’re excited to break new ground,with bold Mars barreling through your exploratory ninth house. But you might have to slow your roll on Saturday, due to a frustrating square between the antsy red planet and murky Neptune in your methodical sixth house. You could be thinking abstractly and painting the picture with broad strokes.")\
+
+name = input("Virgo")
+print("You’re on a tear this Saturday, chasing after big goals while go-getter Mars cruises through your ambition arena. But as red planet clashes with murky Neptune, people might be a distraction, to put it mildly. Flaky or needy types could be the biggest offenders.")
+
+name = input("Capricorn")
+print("How is this going to work? That’s the question you might be asking on Saturday when driven Mars in your systematic sixth house wanders into dubious Neptune’s fog. You’re fired up to get organized but can’t figure out how to execute.")
+
+name = input("Cancer")
+print("Blue-sky thinking could lead you into a gray area on Saturday as impulsive Mars in your twelfth house of imagination spars with nebulous Neptune in your exploratory ninth. It won’t be easy to tell the difference between a doable idea and a fanciful one.")
+
+name = input("Aquarius")
+print("With a one-two “punch” of amorous Venus and communicator Mercury blazing into Sagittarius and your communal eleventh house this Wednesday and Thursday, November 16-17, love, lust and friendship could blend into one sexy brew.")
+
+name = input("Scorpio")
+print("Love and lust are a bewildering blend on Saturday, due to a disorienting square between randy Mars and boundary-challenged Neptune in your passion realms. You might be on the brink of diving in deep with someone, and now you suspect they’re ghosting you.")
+
+name = input("Sagittarius")
+print("It’s never  easy for an honest Archer to stay silent when you’ve got something to say. But with feisty Mars in your relationship zone provoking deceptive Neptune this Saturday, you’re likely to be misunderstood as soon as you open your mouth—even if you’re choosing your words carefully.")
+
+name = input("Leo")
+print("It’s hard to read people’s intentions this Saturday when aggro Mars in your communal eleventh house fights with murky Neptune in your intense, private eighth house. Are they being upfront with you or do they have a hidden agenda? Don’t take everything at face value, no matter how genuine or convincing people appear to be.")
+
+name = input("Gemini")
+print("Shoot your shot, Twin, but don’t get attached to a specific outcome. With fiery Mars motivating you to go for it, you might be feeling unstoppable. But not only is the red planet retrograde in your sign, it’s at odds with vague Neptune on Saturday. The end result is unclear, especially if you’re waiting on a verdict from decision-makers.")
+
+name = input("Pisces")
+print("The future is looking bright on the career front starting this Wednesday, November 16, as auspicious Venus commences her annual tour of Sagittarius and your ambitious tenth house. Your network is indeed your net worth, and with people-oriented Venus here until December 9, your contact database becomes one of your most valuable assets. ")
+
+name = input("Taurus")
+print("Even the most placid Bull will express their stance with noticeable fervor today. Chalk it up to verbal Mercury’s alignment with ardent Venus in your intense eighth house. Your impassioned speech may be completely truthful and still leave a timid type dumbfounded.")
+
+name = input("Aries")
+print("Just because it’s Monday doesn’t mean your mind is on work. Au contraire! As mental Mercury aligns with pleasant Venus in your adventure arena, you’re daydreaming of faraway lands—and perhaps a certain someone who lives in one of those distant locales.")
